3df1a2622001ed1dc26e97907ed14a24a1acdb6e
Jocelyn Fiat <jfiat@eiffel.com>
2012-06-13 19:21:08 +0200

    Better script, do not use default folder without asking.

--- tools/uninstall_ewf.bat
+++ tools/uninstall_ewf.bat
@@ -2,9 +2,27 @@
 setlocal
 set RDCMD= rd /q/s
 
+
 set TMP_TARGET_DIR=%1
-if -%TMP_TARGET_DIR%- == -- goto use_eiffel_library
+if -%TMP_TARGET_DIR%- == -- goto ask_target_dir
 goto start
+
+:ask_target_dir
+echo Please provide a installation directory (target library)
+if -%ISE_LIBRARY%- == -- set ISE_LIBRARY=%EIFFEL_LIBRARY%
+if -%ISE_LIBRARY%- == -- set ISE_LIBRARY=%ISE_EIFFEL%
+if -%EIFFEL_LIBRARY%- == -- set EIFFEL_LIBRARY=%ISE_LIBRARY%
+echo 1: using $EIFFEL_LIBRARY=%EIFFEL_LIBRARY%
+echo 2: using $ISE_LIBRARY=%ISE_LIBRARY%
+echo 3: using current directory=%CD%\ewf
+CHOICE /C 123q /M " > selection:"
+if .%ERRORLEVEL%. == .1. goto use_eiffel_library
+if .%ERRORLEVEL%. == .2. goto use_ise_library
+if .%ERRORLEVEL%. == .3. goto use_current_dir
+echo No target directory were specified, you can pass it using the command line
+echo Usage: install_ewf {target_directory}
+echo Bye ...
+goto end
 
 :use_eiffel_library
 if -%EIFFEL_LIBRARY%- == -- goto use_ise_library
@@ -12,7 +30,12 @@ set TMP_TARGET_DIR=%EIFFEL_LIBRARY%
 goto start
 
 :use_ise_library
+if -%ISE_LIBRARY%- == -- goto use_current_dir
 set TMP_TARGET_DIR=%ISE_LIBRARY%
+goto start
+
+:use_current_dir
+set TMP_TARGET_DIR=%CD%\ewf
 goto start
 
 :start
